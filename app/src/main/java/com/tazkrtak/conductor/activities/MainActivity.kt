b2c07bfc20c8b764440ec1da115a393bd4efa9f5
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2019-10-02 21:57:08 +0200

    Add Simple QR Code Scanner

--- app/src/main/java/com/tazkrtak/conductor/activities/MainActivity.kt
+++ app/src/main/java/com/tazkrtak/conductor/activities/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.tazkrtak.conductor.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 import com.tazkrtak.conductor.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        scan_button.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.captureActivity = ScannerActivity::class.java
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            integrator.setBeepEnabled(false)
+            integrator.initiateScan()
+        }
+
     }
 }
